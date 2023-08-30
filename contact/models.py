1596ff0f9514db70586c1fbc1f100c912aecf4f2
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-08-30 06:10:56 -0300

    INiciando as configurações do modelo e do admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+# blank indica a opcionalidade de se registrar aquele campo, quando se está
+# true quer dizer que não é obrigatorio registrar o campo
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
