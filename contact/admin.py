fcd7a81fc6dfdfbd9f2f3ebd31d1492fa28cc51a
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-09-01 06:41:51 -0300

    Adicionando um gerador de dados, criando uma classe para Categories e fazendo alterações no settings

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from contact.models import Contact
-
+from contact import models
 # Register your models here.
 
 
@@ -12,3 +12,9 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+# type:ignore
+# flake8: noqa
 """
 Django settings for project project.
 
@@ -133,3 +135,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
