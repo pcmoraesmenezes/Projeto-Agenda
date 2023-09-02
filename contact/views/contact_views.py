6e6c83befcc73798ddffef902767f88e398f38fe
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-09-02 07:20:58 -0300

    Adicionando busca

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 # from django.http import Http404
 # Create your views here.
 
@@ -42,5 +43,34 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')[0:10]
+
+    # print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+        'site_tittle': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context
     )
