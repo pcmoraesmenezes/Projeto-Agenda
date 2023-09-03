c360e4926a9a93281704d65a324c0c4427dd12b4
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-09-03 06:31:58 -0300

    Adicionando novas funcionalidades

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 # from django.http import Http404
 # Create your views here.
 
@@ -8,12 +9,16 @@ from django.db.models import Q
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[0:10]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     # print(contacts.query)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_tittle': 'Contatos - '
     }
 
@@ -60,13 +65,17 @@ def search(request):
             Q(phone__icontains=search_value) |
             Q(email__icontains=search_value)
         )\
-        .order_by('-id')[0:10]
+        .order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     # print(contacts.query)
 
     context = {
-        'contacts': contacts,
-        'site_tittle': 'Search - '
+        'page_obj': page_obj,
+        'site_tittle': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
